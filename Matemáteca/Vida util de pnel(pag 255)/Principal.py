d4d3aaa177b136dcdb266a8b03c0ab97b43e3def
jefnerosplays <158824665+jefnerosplays@users.noreply.github.com>
2024-02-14 16:50:49 -0300

    Correção

--- Matemáteca/Vida util de pnel(pag 255)/Principal.py
+++ Matemáteca/Vida util de pnel(pag 255)/Principal.py
@@ -1,5 +1,5 @@
 import math
-pi = 3.141
+pi = 3,141
 r = int(input('Qual é o raio do pnel? '))
 p = 2 * pi * r
 consumo =  7.5* (10 ** -6)
@@ -7,3 +7,7 @@ clc = p / consumo
 d = (p * 100) / (consumo * (10 ** -6))
 lcl = d / 100
 print('{:.1f}'.format(math.ceil(d)))
+
+result = p /7,5 * 10^-6
+sla = d/100 / result
+print(sla)
